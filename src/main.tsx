0ef968835e472ee54a1ec0ba3300585b42b104bf
yehudakahana <yk3222145@gmail.com>
2024-11-03 15:55:43 +0200

    clear main

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { UserProvider, useGlobalUser } from './context/ToDoContext'
+import { UserProvider } from './context/ToDoContext'
 
 createRoot(document.getElementById('root')!).render(
  <UserProvider>
